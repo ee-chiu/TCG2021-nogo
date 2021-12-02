7e1d75acf76932c344e582ae2f0241bed0a657c4
ee-chiu <80535163+ee-chiu@users.noreply.github.com>
2021-12-02 16:19:11 +0800

    update player to MCTS_player

--- nogo.cpp
+++ nogo.cpp
@@ -63,8 +63,8 @@ int main(int argc, const char* argv[]) {
 		summary |= stat.is_finished();
 	}
 
-	player black("name=black " + black_args + " role=black");
-	player white("name=white " + white_args + " role=white");
+	MCTS_player black("name=black " + black_args + " role=black");
+	MCTS_player white("name=white " + white_args + " role=white");
 
 	if (!shell) { // launch standard local games
 		while (!stat.is_finished()) {
